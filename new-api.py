887e162cd58c9d2e1df7d62db29f918cd631e7e2
Karan Kaushik <37307907+karnakaushik@users.noreply.github.com>
2020-05-13 13:37:25 +0530

    Made required changes for using provided API
    The provided API is begin used and data is crawled through that and then pushed into MySQL DB.

--- new-api.py
+++ new-api.py
@@ -1,76 +1,88 @@
 import requests
 import json
 import time
+import urllib.parse
+from mySql import MySql
 
-token = "AoSZVGYBUUQ1/kKDGkPXtECHn6PmNkVglpkl09QeER4="
+token = "sampleToken="
 
-def requestToken():
-    url = "https://public-apis-api.herokuapp.com/api/v1/auth/token"
-    payload = {}
-    headers= {}
-    response = requests.request("GET", url, headers=headers, data = payload)
-    if(response.status_code == 429):
-        time.sleep(int(response.headers['Retry-After']))
-        return requestToken()
-    return str((json.loads(response.text))["token"])
+class Crawler():
 
-
-def getCategory(pageNumber):
-    url = "https://public-apis-api.herokuapp.com/api/v1/apis/categories?page={}".format(pageNumber)
-    global token
-    payload = {}
-    headers= {"Authorization": "Bearer "+str(token)}
-    response = requests.request("GET", url, headers=headers, data = payload)
-    if(response.status_code == 403):
-        token = requestToken()
-        return getCategory(pageNumber)
-    else:
-        print(response.status_code)
+    def requestToken(self):
+        url = "https://public-apis-api.herokuapp.com/api/v1/auth/token"
+        payload = {}
+        headers= {}
+        response = requests.request("GET", url, headers=headers, data = payload)
         if(response.status_code == 429):
             time.sleep(int(response.headers['Retry-After']))
-            return getCategory(pageNumber)
-        category = list((json.loads(response.text))["categories"])
-        return category
+            return self.requestToken()
+        print("New Token Generated")
+        return str((json.loads(response.text))["token"])
+
+
+    def getCategory(self,pageNumber):
+        url = "https://public-apis-api.herokuapp.com/api/v1/apis/categories?page={}".format(pageNumber)
+        global token
+        payload = {}
+        headers= {"Authorization": "Bearer "+str(token)}
+        response = requests.request("GET", url, headers=headers, data = payload)
+        if(response.status_code == 403):
+            token = self.requestToken()
+            return self.getCategory(pageNumber)
+        else:
+            print(response.status_code)
+            if(response.status_code == 429):
+                time.sleep(int(response.headers['Retry-After']))
+                return self.getCategory(pageNumber)
+            category = list((json.loads(response.text))["categories"])
+            return category
 
 
 
-def getDetails(pageNumber,category):
-    url = """https://public-apis-api.herokuapp.com/api/v1/apis/entry?page={}&category={}""".format(pageNumber,category)
-    global token
-    payload = {}
-    headers= {"Authorization": "Bearer "+str(token)}
-    response = requests.request("GET", url, headers=headers, data = payload)
-    if(response.status_code == 403):
-        token = requestToken()
-        return getDetails(pageNumber, category)
-    else:
-        print(response.status_code)
-        if(response.status_code == 429):
-            time.sleep(int(response.headers['Retry-After']))
-            return getDetails(pageNumber,category)
-        apis = list((json.loads(response.text))["categories"])
-        return apis
+    def getDetails(self,pageNumber,category):
+        url = """https://public-apis-api.herokuapp.com/api/v1/apis/entry?page={}&category={}""".format(pageNumber,category)
+        global token
+        payload = {}
+        headers= {"Authorization": "Bearer "+str(token)}
+        response = requests.request("GET", url, headers=headers, data = payload)
+        if(response.status_code == 403):
+            token = self.requestToken()
+            return self.getDetails(pageNumber, category)
+        else:
+            print(response.status_code)
+            if(response.status_code == 429):
+                time.sleep(int(response.headers['Retry-After']))
+                return self.getDetails(pageNumber,category)
+            apis = list((json.loads(response.text))["categories"])
+            return apis
 
 
-# print(requestToken())
-categories = []
-for x in range(1,11):
-    listCat = getCategory(x)
-    if(len(listCat) == 0):
-        print("1")
-        break
-    else:
-        categories.extend(listCat)
-print(categories)
 
-
-for x in categories:
-    details = []
-    print(x)
-    for p in range(1,5):
-        listCat = getDetails(p,x)
+if __name__ == "__main__":
+    sql = MySql()
+    webCrawler = Crawler()
+    categories = []
+    for x in range(1,11):
+        listCat = webCrawler.getCategory(x)
         if(len(listCat) == 0):
             break
         else:
-            details.extend(listCat)
-    print(details)
+            categories.extend(listCat)
+    print(categories)
+
+
+    for category in categories:
+        details = []
+        sql.createTable(category)
+        category = urllib.parse.quote(category)
+        for p in range(1,10):
+            listCat = webCrawler.getDetails(p,category)
+            if(len(listCat) == 0):
+                break
+            else:
+                details.extend(listCat)
+        category = urllib.parse.unquote(category)
+        for i in details:
+            sql.insert(i,category)
+
+
